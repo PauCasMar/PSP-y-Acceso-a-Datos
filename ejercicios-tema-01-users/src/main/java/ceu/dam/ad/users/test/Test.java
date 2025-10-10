37349848b418af87eb20fe128debed3faa77d57d
Paula Castro <epaulacastromartinez@gmail.com>
2025-10-10 10:57:30 +0200

    Update Test.java

--- ejercicios-tema-01-users/src/main/java/ceu/dam/ad/users/test/Test.java
+++ ejercicios-tema-01-users/src/main/java/ceu/dam/ad/users/test/Test.java
@@ -20,43 +20,55 @@ public class Test {
 		System.out.println(" 1- Crear usuario \n 2- Cambiar contraseña\n 3- Login \n 4- Consultar usuario");
 		Integer num = sc.nextInt();
 		try {
-			do {
-				switch (num) {
 
-				case 1:
-					User usuario = new User();
-					usuario.setUsername("Pepe");
-					usuario.setEmail("email@pepe.com");
-					usuario.setPassword("pepemola");
+			switch (num) {
 
-					userVicio.createUser(usuario);
-					System.out.println("Usuario introducido correctamente");
-					break;
-				case 2:
-					User usuario2 = new User();
-					String oldPass = usuario2.getPassword();
-					String newPass = "vayaPassRara2020";
-					Long id = (long) 1;
+			case 1:
+				//Crear usuario
+				User usuario = new User();
+				usuario.setUsername("Manolo");
+				usuario.setEmail("email@mauel.com");
+				usuario.setPassword("manumola");
+				userVicio.createUser(usuario);
+					
+				break;
+			case 2:
+				//Cambiar contraseña
+				User usuario2 = new User();
+				Long id = (long) 2;
+				usuario2 = userVicio.getUser(id);
+				String oldPass = usuario2.getPassword();
+				String newPass = "fadssas";
+				userVicio.changePassword(id, oldPass, newPass);
 
-					userVicio.changePassword(id, oldPass, newPass);
-					break;
+				break;
 
-				case 3:
-					break;
+			case 3:
+				//Login y actualizar fecha ultimo login
+				User user3 = new User();
+				user3.setUsername("Laura");
+				user3.setPassword("fadssas");
+				userVicio.login(user3.getUsername(), user3.getPassword());
+							
+				break;
 
-				case 4:
-					break;
-				case 5:
-					break;
-				default:
-					System.out.println("Opción incorrecta");
-				}
+			case 4:
+				//Info de usuario
+				Long id2 = (long) 3;
+				User user4= userVicio.getUser(id2);
+				System.out.println(user4);				
+				
+				break;
+			
+			default:
+				System.out.println("Opción incorrecta");
+			}
 
-			} while (num < 5);
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		};
 		sc.close();
 	}
 
 }
+
